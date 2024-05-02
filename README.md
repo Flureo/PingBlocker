265c89e0f6f8a4cf7bc608085a30045bb2925660
McFlurry <102613572+McFlurrYT@users.noreply.github.com>
2024-05-02 12:23:28 -0400

    Config Update!

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # PingBlocker
- 
+
+    PingBlocker is a Minecraft plugin that hides servers from server scanners.
+Server scanners work by first scanning every ip with 25565 open. Then they scan 
+all of those ips to see if its a Minecraft. To do this they send a ping request
+packet to the server that sends them back the a json file format status of the 
+server. It sends back stuff like all the players online and the motd of the server.
+This is how you get the status of the server on the server list in Minecraft. But
+if the server doesn't respond to the ping then the server scanner wouldn't know
+if its a Minecraft server or not. This is how PingBlocker protects your server.
+
+# Config
+
+```yml
+# Don't change this unless you update this plugin.
+version: '1.0'
+
+# If true, the plugin will log debug info.
+debug: false
+
+# There are three different modes 'BLOCK', 'CLOAK', AND 'OFF'.
+# 'BLOCK' doesn't respond to any pings.
+# 'CLOAK' responds with a fake ping.
+# 'OFF' turns the plugin OFF.
+mode: 'BLOCK'
+
+# If true, whenever a player leaves the server the player's ip is added to a list.
+# And all ips added to the list will be able to ping the server.
+respond-after-player-quits: true
+
+# Each 1 whole number is a whole minute.
+# If 0, the timer will be disabled.
+# After this timer is over it will clear all ips added to the respond-after-player-join list.
+cached-address-timer: 30
+
+# All ips added to allowed-addresses below will always be able to ping the server.
+allowed-addresses:
+  - '127.0.0.1'
+
+# When 'CLOAK' mode is selected this will be the version displayed in the fake response ping.
+cloak-version: "Null"
+
+# When 'CLOAK' mode is selected this will be the protocol version displayed in the fake response ping.
+cloak-protocol-version: 0
+
+# When 'CLOAK' mode is selected this will be the motd displayed in the fake response ping.
+cloak-motd: "A Minecraft Server"
+
+# When 'CLOAK' mode is selected this will be the player count displayed in the fake response ping.
+cloak-player-count: 0
+cloak-player-max: 20
+```
+
+# Installation
+
+# Build
--- src/main/java/me/mcflurry/pingblocker/PingBlocker.java
+++ src/main/java/me/mcflurry/pingblocker/PingBlocker.java
@@ -4,26 +4,28 @@ import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.util.CachedServerIcon;
 
-import java.util.Objects;
+import java.util.*;
 
 public final class PingBlocker extends JavaPlugin implements Listener {
+    private final List<String> list_of_addresses = new ArrayList<String>();
 
-    private String[] list_of_addresses = new String[0];
+    private void cached_reset() {
+        list_of_addresses.addAll(getConfig().getStringList("allowed-addresses"));
 
-    private void list_reset() {
-        if(getConfig().getBoolean("list") && getConfig().getBoolean("list-reset")) {
+        if(getConfig().getBoolean("respond-after-player-quits") && getConfig().getInt("cached-address-timer") <= 0) {
             BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
             scheduler.scheduleSyncDelayedTask(this, new Runnable() {
                 @Override
                 public void run() {
-                    list_of_addresses = new String[0];
-                    list_reset();
+                    list_of_addresses.clear();
+                    cached_reset();
                 }
-            }, 60*getConfig().getInt("list-reset-timer")*20L);
+            }, 60*getConfig().getInt("cached-address-timer")*20L);
         }
     }
 
@@ -33,40 +35,41 @@ public final class PingBlocker extends JavaPlugin implements Listener {
 
         saveDefaultConfig();
 
-        if(getConfig().getBoolean("list") && getConfig().getBoolean("list-reset")) list_reset();
+        if(!Objects.equals(getConfig().getString("version"), "1.0")) {
+            getLogger().warning("Error: Invalid Version.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+
+        if(!Objects.equals(getConfig().getString("mode"), "OFF")) cached_reset();
+        if(!Objects.equals(getConfig().getString("mode"), "OFF") && !Objects.equals(getConfig().getString("mode"), "BLOCK") && !Objects.equals(getConfig().getString("mode"), "CLOAK")) {
+            getLogger().warning("Error: Invalid Mode.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
 
     @EventHandler
-    public void onPlayerLoginEvent(PlayerLoginEvent event) {
-        boolean hasIP = false;
-        for (String s : list_of_addresses) if (s.equals(event.getAddress().toString())) hasIP = true;
-
-        if(getConfig().getBoolean("list") && !hasIP) {
-            String[] clone_buffer = list_of_addresses;
-            list_of_addresses = new String[list_of_addresses.length + 1];
-
-            System.arraycopy(clone_buffer, 0, list_of_addresses, 0, clone_buffer.length);
-            list_of_addresses[list_of_addresses.length - 1] = String.valueOf(event.getAddress());
-        }
+    public void OnPlayerQuitEvent(PlayerQuitEvent event) {
+        if(getConfig().getBoolean("respond-after-player-quits") && !list_of_addresses.contains(Objects.requireNonNull(event.getPlayer().getAddress()).toString().substring(0, event.getPlayer().getAddress().toString().indexOf(":")).replace("/", "")))
+            list_of_addresses.add(event.getPlayer().getAddress().toString().substring(0, event.getPlayer().getAddress().toString().indexOf(":")).replace("/", ""));
     }
 
     @EventHandler
     public void onPaperServerListPingEvent(PaperServerListPingEvent event) {
-        Bukkit.getLogger().info(event.getAddress() + " is attempting to ping the server.");
+        if(getConfig().getBoolean("debug")) Bukkit.getLogger().info(event.getAddress().toString().replace("/", "") + " is attempting to ping the server.");
 
-        boolean hasIP = false;
-        for (String s : list_of_addresses) if (s.equals(event.getAddress().toString())) hasIP = true;
-
-        if(!hasIP && Objects.equals(getConfig().getString("mode"), "BLOCK") && !event.isCancelled()) {
+        if(!list_of_addresses.contains(event.getAddress().toString().replace("/", "")) && Objects.equals(getConfig().getString("mode"), "BLOCK") && !event.isCancelled()) {
             event.setCancelled(true);
         }
-        else if(!hasIP && Objects.equals(getConfig().getString("mode"), "CLOAK") && !event.isCancelled()) {
-            event.setNumPlayers(0);
-            event.setMaxPlayers(20);
+        else if(!list_of_addresses.contains(event.getAddress().toString().replace("/", "")) && Objects.equals(getConfig().getString("mode"), "CLOAK") && !event.isCancelled()) {
+            event.setNumPlayers(getConfig().getInt("cloak-player-count"));
+            event.setMaxPlayers(getConfig().getInt("cloak-player-max"));
+
+            event.setVersion(Objects.requireNonNull(getConfig().getString("cloak-version")));
+            event.setProtocolVersion(getConfig().getInt("cloak-protocol-version"));
+
+            event.setMotd(Objects.requireNonNull(getConfig().getString("cloak-motd")));
+
             event.setServerIcon(null);
-            event.setVersion("null");
-            event.setProtocolVersion(0);
-            event.setMotd("A Minecraft Server");
         }
     }
 }
